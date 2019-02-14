48b01a2741e46f7632af5bcf2fe9c060d1871e0d
Arslan <esiredrise@gmail.com>
2019-02-14 23:32:49 +0600

    pre-final

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ import numpy as np
 import os
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+        image = image.convert_alpha()
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        return image
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+
+# создание мира
+pygame.init()
+all_sprites = pygame.sprite.Group()
+screen = pygame.display.set_mode((1280, 720))
+objects = {
+    0: load_image("air.png"),
+    1: load_image("dirt.png"),
+    2: load_image("stone.png"),
+    3: load_image("cobblestone.png"),
+    4: load_image("wood.png"),
+    5: load_image("sand.png"),
+    6: load_image("leaves.png"),
+    7: load_image("gravy.png"),
+    8: load_image("diamond_ore.png")
+}
+
+
 class World:
     def __init__(self, typ="standard"):  # на будущее
         self.table = np.zeros(1000000, dtype=int)  # заполняем воздухом
@@ -52,21 +84,6 @@ def ov_mod(n):
             return int(n // 1) - 1
         else:
             return int(n // 1) + 1
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-        image = image.convert_alpha()
-        if colorkey is not None:
-            if colorkey is -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        return image
-    except pygame.error as message:
-        print('Cannot load image:', name)
-        raise SystemExit(message)
 
 
 class Inventory:
@@ -123,9 +140,14 @@ class Button(pygame.sprite.Sprite):
             return False
 
 
+class MenuError(Exception):
+    pass
+
+
 def pause(screen):
     running = True
-    button_continue = Button("Continue", 540, 345, screen)
+    button_continue = Button("Continue", 540, 305, screen)
+    return_menu = Button("Return to the menu", 540, 385, screen)
     while running:
         screen.fill(pygame.color.Color("grey"))
         for event in pygame.event.get():
@@ -135,158 +157,195 @@ def pause(screen):
                 if event.key == pygame.K_ESCAPE:
                     running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                button_continue.is_clicked(event.pos)
+                if button_continue.is_clicked(event.pos):
+                    running = False
+                elif return_menu.is_clicked(event.pos):
+                    raise MenuError
         button_continue.draw_()
+        return_menu.draw_()
         pygame.display.flip()
 
 
-# создание мира
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-objects = {
-    0: load_image("air.png"),
-    1: load_image("dirt.png"),
-    2: load_image("stone.png"),
-    3: load_image("cobblestone.png"),
-    4: load_image("wood.png"),
-    5: load_image("sand.png"),
-    6: load_image("leaves.png"),
-    7: load_image("gravy.png"),
-    8: load_image("diamond_ore.png")
-}
-character = load_image("char.png")
-screen.fill((0, 0, 0))
-all_sprites = pygame.sprite.Group()
-main_char = Character(2, 2)
-clock = pygame.time.Clock()
-screen.fill((255, 255, 255))
-world1 = World()
-world1.append("main_char", (main_char, main_char.x + 500*20, - main_char.y + 500*20))
-running = True
-inventory = Inventory(main_char)
-# для упрошения кода
-w, h = pygame.display.get_surface().get_size()
-ch_w = main_char.weight
-ch_h = main_char.height
-# запуск
-while running:
+def main():
+    character = load_image("char.png")
     screen.fill((0, 0, 0))
-    if main_char.v_v < 0:
-        main_char.direction[1] = 1
-    else:
-        main_char.direction[1] = 0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            # при нажатии его горизонтальное ускорение изменятся
-            if event.key == pygame.K_d:
-                main_char.a_h = 100
-                main_char.direction[0] = 1
-            elif event.key == pygame.K_a:
-                main_char.a_h = -100
-                main_char.direction[0] = 0
-            elif event.key == pygame.K_SPACE:
-                # если он стоит на земле то при нажатии на пробел его вертикальная скорость приравнивается 200
-                if main_char.status == "on_ground":
-                    main_char.v_v = 200
-            elif event.key == pygame.K_1:
-                inventory.equipped = 0
-            elif event.key == pygame.K_2:
-                inventory.equipped = 1
-            elif event.key == pygame.K_3:
-                inventory.equipped = 2
-            elif event.key == pygame.K_4:
-                inventory.equipped = 3
-            elif event.key == pygame.K_5:
-                inventory.equipped = 4
-            elif event.key == pygame.K_6:
-                inventory.equipped = 5
-            elif event.key == pygame.K_7:
-                inventory.equipped = 6
-            elif event.key == pygame.K_8:
-                inventory.equipped = 7
-            elif event.key == pygame.K_e:
-                if inventory.opened:
-                    inventory.close()
-                else:
-                    inventory.open()
-            elif event.key == pygame.K_ESCAPE:
-                pause(screen)
-        if event.type == pygame.KEYUP:
-            # при отжатии в горизонтальные переменные = 0
-            if event.key == pygame.K_d:
-                main_char.v_h = 0
-                main_char.a_h = 0
-            if event.key == pygame.K_a:
-                main_char.v_h = 0
-                main_char.a_h = 0
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if not inventory.opened:
-                cell = int(world1.lst_objects["main_char"][1] + event.pos[0] - 640)//20, int(world1.lst_objects["main_char"][2] + event.pos[1] - 360)//20
-                if event.button == 1:
-                    world1.table[cell[1], cell[0]] = 0
-                elif event.button == 3:
-                    world1.table[cell[1], cell[0]] = inventory.equipped + 1
-    # проверка на то что игрок стоит на земле или же в воздухе
-    try:
-        if world1.table[500 - int(main_char.y // 20) - 2][int(main_char.x // 20) + 500] != 0:
-            main_char.v_v = 0
-        if world1.table[500 - int(main_char.y // 20) - 1][int((main_char.x - 10) // 20) + 500] != 0 or \
-                world1.table[500 - int(main_char.y // 20) - 2][int((main_char.x - 10) // 20) + 500] != 0:
-            if main_char.direction[0] == 0:
-                main_char.v_h = 0
-        if world1.table[500 - int(main_char.y // 20) - 1][int((main_char.x + 10) // 20) + 500] != 0 or \
-                world1.table[500 - int(main_char.y // 20) - 2][int((main_char.x + 10) // 20) + 500] != 0:
-            if main_char.direction[0] != 0:
-                main_char.v_h = 0
-        if world1.table[500 - int(main_char.y // 20)][int(main_char.x // 20) + 500] != 0:
-            main_char.status = "on_ground"
-            if main_char.v_v < 0:
-                main_char.hp += int(main_char.v_v / 400)
-            if main_char.hp < 0:
-                main_char.hp = 0
-            main_char.a_v = 0
-            if main_char.direction[1] != 0:
+    main_char = Character(2, 2)
+    clock = pygame.time.Clock()
+    screen.fill((255, 255, 255))
+    world1 = World()
+    world1.append("main_char", (main_char, main_char.x + 500*20, - main_char.y + 500*20))
+    running = True
+    inventory = Inventory(main_char)
+    # для упрошения кода
+    w, h = pygame.display.get_surface().get_size()
+    ch_w = main_char.weight
+    ch_h = main_char.height
+    # запуск
+    while running:
+        screen.fill((0, 0, 0))
+        if main_char.v_v < 0:
+            main_char.direction[1] = 1
+        else:
+            main_char.direction[1] = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                # при нажатии его горизонтальное ускорение изменятся
+                if event.key == pygame.K_d:
+                    main_char.a_h = 100
+                    main_char.direction[0] = 1
+                elif event.key == pygame.K_a:
+                    main_char.a_h = -100
+                    main_char.direction[0] = 0
+                elif event.key == pygame.K_SPACE:
+                    # если он стоит на земле то при нажатии на пробел его вертикальная скорость приравнивается 200
+                    if main_char.status == "on_ground":
+                        main_char.v_v = 200
+                elif event.key == pygame.K_1:
+                    inventory.equipped = 0
+                elif event.key == pygame.K_2:
+                    inventory.equipped = 1
+                elif event.key == pygame.K_3:
+                    inventory.equipped = 2
+                elif event.key == pygame.K_4:
+                    inventory.equipped = 3
+                elif event.key == pygame.K_5:
+                    inventory.equipped = 4
+                elif event.key == pygame.K_6:
+                    inventory.equipped = 5
+                elif event.key == pygame.K_7:
+                    inventory.equipped = 6
+                elif event.key == pygame.K_8:
+                    inventory.equipped = 7
+                elif event.key == pygame.K_e:
+                    if inventory.opened:
+                        inventory.close()
+                    else:
+                        inventory.open()
+                elif event.key == pygame.K_ESCAPE:
+                    try:
+                        pause(screen)
+                    except MenuError:
+                        running = False
+            if event.type == pygame.KEYUP:
+                # при отжатии в горизонтальные переменные = 0
+                if event.key == pygame.K_d:
+                    main_char.v_h = 0
+                    main_char.a_h = 0
+                if event.key == pygame.K_a:
+                    main_char.v_h = 0
+                    main_char.a_h = 0
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if not inventory.opened:
+                    cell = int(world1.lst_objects["main_char"][1] + event.pos[0] - 640)//20, int(world1.lst_objects["main_char"][2] + event.pos[1] - 360)//20
+                    if event.button == 1:
+                        world1.table[cell[1], cell[0]] = 0
+                    elif event.button == 3:
+                        world1.table[cell[1], cell[0]] = inventory.equipped + 1
+        # проверка на то что игрок стоит на земле или же в воздухе
+        try:
+            if world1.table[500 - int(main_char.y // 20) - 2][int(main_char.x // 20) + 500] != 0:
                 main_char.v_v = 0
-        elif world1.table[500 - int(main_char.y // 20)][int(main_char.x // 20) + 500] == 0:
-            main_char.status = "in_air"
-            # его вертикальное ускорение = g
-            main_char.a_v = 0 - 9.8 * 30
-    except IndexError:
-        pass
+            if world1.table[500 - int(main_char.y // 20) - 1][int((main_char.x - 10) // 20) + 500] != 0 or \
+                    world1.table[500 - int(main_char.y // 20) - 2][int((main_char.x - 10) // 20) + 500] != 0:
+                if main_char.direction[0] == 0:
+                    main_char.v_h = 0
+            if world1.table[500 - int(main_char.y // 20) - 1][int((main_char.x + 10) // 20) + 500] != 0 or \
+                    world1.table[500 - int(main_char.y // 20) - 2][int((main_char.x + 10) // 20) + 500] != 0:
+                if main_char.direction[0] != 0:
+                    main_char.v_h = 0
+            if world1.table[500 - int(main_char.y // 20)][int(main_char.x // 20) + 500] != 0:
+                main_char.status = "on_ground"
+                if main_char.v_v < 0:
+                    main_char.hp += int(main_char.v_v / 400)
+                if main_char.hp < 0:
+                    main_char.hp = 0
+                main_char.a_v = 0
+                if main_char.direction[1] != 0:
+                    main_char.v_v = 0
+            elif world1.table[500 - int(main_char.y // 20)][int(main_char.x // 20) + 500] == 0:
+                main_char.status = "in_air"
+                # его вертикальное ускорение = g
+                main_char.a_v = 0 - 9.8 * 30
+        except IndexError:
+            pass
+        try:
+            if main_char.a_h != 0 or main_char.v_h != 0:
+                # уравнение горизонтальной скорости
+                main_char.x = main_char.x + main_char.v_h * clock.get_time()/1000
+                # ограничение в скорости 40 метров в секунду или 800 пикселей в с
+                if abs(main_char.v_h) >= 800:
+                    pass
+                else:
+                    # уравнение движение по горизонтальной оси
+                    main_char.v_h = main_char.v_h + main_char.a_h * clock.get_time() / 1000
+            # тоже самое только для вертикальной оси
+            if main_char.a_v != 0 or main_char.v_v != 0:
+                main_char.y = main_char.y + main_char.v_v * clock.get_time() / 1000 + \
+                    (main_char.a_v * ((clock.get_time() / 1000) ** 2)) / 2
+                if abs(main_char.v_v) >= 800:
+                    pass
+                else:
+                    main_char.v_v = main_char.v_v + main_char.a_v * clock.get_time() / 1000
+            world1.lst_objects["main_char"] = (main_char, main_char.x + 500*20, - main_char.y + 500*20)
+        except IndexError:
+            pass
+        # прорисовка мира
+        for i in range(38):
+            for j in range(66):
+                cell = world1.table[ov_mod(world1.lst_objects["main_char"][2] / 20) + (i - 19)][
+                    int(world1.lst_objects["main_char"][1] / 20) + (j - 33)]
+                screen.blit(objects[cell], (int((j - 1)*20 - main_char.x % 20), int((i - 1)*20 + main_char.y % 20)))
+        # отрисовка персонажа
+        screen.blit(character, (w // 2 - ch_w // 2, h // 2 - ch_h // 2))
+        inventory.draw(scren=screen)
+        pygame.display.flip()
+        # лог для проверки достоверности того что мы видем
+        print(main_char.v_v)
+        clock.tick(60)
+
+
+def rule():
+    running = True
+    fon = load_image("rule.png")
+    screen.blit(fon, (0, 0))
+    back = Button("Back", 0, 0, screen)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if back.is_clicked(event.pos):
+                    running = False
+        back.draw_()
+        pygame.display.flip()
+
+
+def start_screen():
     try:
-        if main_char.a_h != 0 or main_char.v_h != 0:
-            # уравнение горизонтальной скорости
-            main_char.x = main_char.x + main_char.v_h * clock.get_time()/1000
-            # ограничение в скорости 40 метров в секунду или 800 пикселей в с
-            if abs(main_char.v_h) >= 800:
-                pass
-            else:
-                # уравнение движение по горизонтальной оси
-                main_char.v_h = main_char.v_h + main_char.a_h * clock.get_time() / 1000
-        # тоже самое только для вертикальной оси
-        if main_char.a_v != 0 or main_char.v_v != 0:
-            main_char.y = main_char.y + main_char.v_v * clock.get_time() / 1000 + \
-                (main_char.a_v * ((clock.get_time() / 1000) ** 2)) / 2
-            if abs(main_char.v_v) >= 800:
-                pass
-            else:
-                main_char.v_v = main_char.v_v + main_char.a_v * clock.get_time() / 1000
-        world1.lst_objects["main_char"] = (main_char, main_char.x + 500*20, - main_char.y + 500*20)
-    except IndexError:
+        running = True
+        play_button = Button("Play", 540, 225, screen)
+        rule_button = Button("Rules", 540, 345, screen)
+        exit_button = Button("Exit", 540, 475, screen)
+        while running:
+            screen.fill(pygame.color.Color("grey"))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if play_button.is_clicked(event.pos):
+                        main()
+                    elif rule_button.is_clicked(event.pos):
+                        rule()
+                    elif exit_button.is_clicked(event.pos):
+                        pygame.quit()
+            play_button.draw_()
+            rule_button.draw_()
+            exit_button.draw_()
+            pygame.display.flip()
+    except pygame.error:
         pass
-    # прорисовка мира
-    for i in range(38):
-        for j in range(66):
-            cell = world1.table[ov_mod(world1.lst_objects["main_char"][2] / 20) + (i - 19)][
-                int(world1.lst_objects["main_char"][1] / 20) + (j - 33)]
-            screen.blit(objects[cell], (int((j - 1)*20 - main_char.x % 20), int((i - 1)*20 + main_char.y % 20)))
-    # отрисовка персонажа
-    screen.blit(character, (w // 2 - ch_w // 2, h // 2 - ch_h // 2))
-    inventory.draw(scren=screen)
-    pygame.display.flip()
-    # лог для проверки достоверности того что мы видем
-    print(main_char.v_v)
-    clock.tick(60)
-pygame.quit()
+
+
+start_screen()
